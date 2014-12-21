51edcbd221fe56d9a0bca87cb165d7687a9420c7
Augie Schwer <augie@Angelas-MacBook-Air.local>
2014-12-21 15:53:46 -0800

    Added ping/pong and ackbar stub support

--- bot.js
+++ bot.js
@@ -1,57 +1,37 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
+var ackbar = require('./ackbar.js');
 
 var botID = process.env.BOT_ID;
+var botName = process.env.BOT_NAME;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
 
-  if(request.text && botRegex.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage();
-    this.res.end();
-  } else {
-    console.log("don't care");
-    this.res.writeHead(200);
-    this.res.end();
+  var request = JSON.parse(this.req.chunks[0]);
+
+  var pingRegex = new RegExp('^\\@?' + botName + ' ping', 'i');
+  var ackbarRegex = /it[']?s a trap\b/i;
+
+  this.res.writeHead(200);
+
+  console.log('got ' + request.text );
+
+  if(pingRegex.test(request.text)) {
+	  postMessage('PONG');
+  } else if(ackbarRegex.test(request.text)) {
+	  var ackbar_url = ackbar();
+	  postMessage(ackbar_url);
   }
+
+  this.res.end();
+
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
+function postMessage(msg) {
 
-  botResponse = cool();
+  var botResponse = msg;
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+  console.log('sent ' + botResponse);
 
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
 }
-
 
 exports.respond = respond;

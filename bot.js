8f425cc75d05e5f9a7b15d874f2b9ad49d673027
Augie Schwer <augie@Angelas-MacBook-Air.local>
2014-12-21 16:28:42 -0800

    Added sending messages to GroupMe and bumped the version

--- bot.js
+++ bot.js
@@ -13,13 +13,16 @@ function respond() {
 
   this.res.writeHead(200);
 
-  console.log('got ' + request.text );
 
   if(pingRegex.test(request.text)) {
+	  console.log('got ' + request.text );
 	  postMessage('PONG');
   } else if(ackbarRegex.test(request.text)) {
+	  console.log('got ' + request.text );
 	  var ackbar_url = ackbar();
 	  postMessage(ackbar_url);
+  } else {
+	  console.log("don't care");
   }
 
   this.res.end();
@@ -30,8 +33,36 @@ function postMessage(msg) {
 
   var botResponse = msg;
 
-  console.log('sent ' + botResponse);
+  var options = {
+	  hostname: 'api.groupme.com',
+	  path: '/v3/bots/post',
+	  method: 'POST'
+  };
 
+  body = {
+	  "bot_id" : botID,
+	  "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+	  if(res.statusCode == 202) {
+		  //neat
+	  } else {
+		  console.log('rejecting bad status code ' + res.statusCode);
+	  }
+  });
+
+  botReq.on('error', function(err) {
+	  console.log('error posting message '  + JSON.stringify(err));
+  });
+
+  botReq.on('timeout', function(err) {
+	  console.log('timeout posting message '  + JSON.stringify(err));
+  });
+
+  botReq.end(JSON.stringify(body));
 }
 
 exports.respond = respond;
